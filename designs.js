c9a8ff5966f4db731742129e7050574aaabb6a4c
Mariia Kasianova <kasyanovamg@gmail.com>
2018-01-01 21:15:27 +0400

    feat: drag and draw

--- designs.js
+++ designs.js
@@ -4,17 +4,44 @@
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
-	const rows = $("#input_height").val();
-	const cols = $("#input_width").val();
-	for (let i = 0 ; i < cols ; i++) {
+	let rows = $('#input_height').val();
+	let cols = $('#input_width').val();
+
+	//remove the previous designs
+	$('#pixel_canvas').children().remove();
+
+	//loops to draw the grid
+	for (let i = 0 ; i < rows ; i++) {
     	$('#pixel_canvas').append('<tr></tr>');
-    	for (let j = 0 ; j< rows; j++) {
+    	for (let j = 0 ; j< cols; j++) {
         	$('#pixel_canvas').children().last().append('<td></td>');
     	}
     }	
 }
+function makeColor() {
+	var draw = false;
+	$('#pixel_canvas').on('mousedown', 'td', function(){
+		draw = true; 
+		var color = $('#colorPicker').val();
+		$(this).css('background-color', color);
+	});
+	$('#pixel_canvas').on('mouseup', 'td', function() {
+		draw=false;
+	});
+	$('#pixel_canvas').on('mouseenter', 'td', function(){
+		if(draw) {
+			var color = $('#colorPicker').val();
+			$(this).css('background-color', color); 
+		}
+		else {
+			return;
+		}
+	});
+}
 
+//calling functions on submit
 $('form').on('submit', function(e) {	
 	e.preventDefault();
 	makeGrid();
+	makeColor()
 });
