6fa17ca4402b3a3d54a7a676dc39aab646fc25b5
Mariia Kasianova <kasyanovamg@gmail.com>
2018-01-02 20:48:30 +0400

    feat: Deleting color with right click while draging

--- designs.js
+++ designs.js
@@ -47,9 +47,63 @@ function makeColor() {
 	});
 }
 
+//deleting colors with right click
+function deleteColor() {
+	var draw_del = false;
+	var bk_color = '';
+	document.oncontextmenu = function() {
+	return false;
+	};
+
+	$('#pixel_canvas').on('mousedown', 'td', function(event) {
+		switch (event.which) {
+			case 1:
+				draw_del = false;
+				break;
+			case 2:
+				draw_del = false;
+				break;
+			case 3:
+				draw_del = true; 
+				$(this).css('background-color', bk_color);
+				break;
+		}	
+	});
+
+	$('#pixel_canvas').on('mouseup', 'td', function(event) {
+		switch (event.which) {
+			case 1:
+				draw_del = false;
+				break;
+			case 2:
+				draw_del = false;
+				break;
+			case 3:
+				draw_del = false;
+				break;
+		}
+		
+	});
+	$('#pixel_canvas').on('mouseenter', 'td', function(){
+		if(draw_del) {
+			$(this).css('background-color', bk_color); 
+		}
+		else {
+			return;
+		}
+
+	//prevents coloring after reentering the grid 
+	$('#pixel_canvas').on('mouseleave', function(){ 
+		draw_del = false;
+		return;	
+	});
+	});
+}
+
 //calling functions on submit
 $('form').on('submit', function(e) {	
 	e.preventDefault();
 	makeGrid();
-	makeColor()
+	makeColor();
+	deleteColor();
 });
